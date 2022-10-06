00b235a2322c75d64171be409722ac4c20a4db56
israsuniverse <103148342+israsuniverse@users.noreply.github.com>
2022-10-06 00:08:49 -0700

    Add files via upload

---
+++ average.py
@@ -0,0 +1,31 @@
+# project-2a
+
+Write a program that asks the user for five numbers and then prints out the average of those numbers.  When you run your program, it should match the following format:
+```
+Please enter five numbers.
+-2.4
+5.1
+6.0
+123.8
+-19.0
+The average of those numbers is:
+22.7
+```
+The file must be named: average.py
+print("Enter five integers")
+#asking user
+x = 0
+#define x as 0
+#for loop
+for i in range(5) : 
+s = int(input())
+#taking input
+x += s 
+#adding to s
+print("the average of those numbers is:", x/5)
+#printed average
+
+
+
+
+
